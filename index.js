aeb1ac0d806ff300236e4765819f682cfb3545fb
unknown <showlinakter77@gmail.com>
2024-06-22 23:20:56 +0600

    update coffee section complete

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function run() {
 
         const coffeeCollection = client.db('coffeeDB').collection('coffee');
 
-        app.get('/coffee', async(req,res) =>{
+        app.get('/coffee', async (req, res) => {
             const cursor = coffeeCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -41,10 +41,38 @@ async function run() {
             res.send(result);
         })
 
-        app.delete('/coffee/:id',async(req,res) =>{
+        app.put('/coffee/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedCoffee= req.body;
+
+            const coffee = {
+                $set: {
+                    name: updatedCoffee.name,
+                    quantity: updatedCoffee.quantity,
+                    supplier: updatedCoffee.supplier,
+                    test: updatedCoffee.test,
+                    category: updatedCoffee.category,
+                    details: updatedCoffee.details,
+                    photo: updatedCoffee.photo
+                }
+            }
+            const result = await coffeeCollection.updateOne(filter,coffee,options);
+            res.send(result);
+        })
+
+        app.delete('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
             const result = await coffeeCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.get('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeeCollection.findOne(query);
             res.send(result);
         })
 
